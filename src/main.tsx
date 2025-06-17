9fd7b2c16815fb6a63804cd69a55f318241ef18d
TogetherHumanity <DAVID@TOGETHERHUMANITY.COM>
2025-06-16 18:29:16 -0700

    Delete src directory

--- src/main.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-
-const App = () => <h1>Hello from Vyana</h1>
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
