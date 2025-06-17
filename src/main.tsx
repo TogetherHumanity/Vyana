7f574b5aadabeb9257f9badb49d85f4fd7ceab7c
TogetherHumanity <DAVID@TOGETHERHUMANITY.COM>
2025-06-16 18:27:59 -0700

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const App = () => <h1>Hello from Vyana</h1>
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
