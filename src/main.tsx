9f7f5c3b9ae70360def10dd26ffba58555bd5d5a
TogetherHumanity <DAVID@TOGETHERHUMANITY.COM>
2025-06-16 19:46:16 -0700

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App = () => {
+  return <h1 style={{ color: 'white', textAlign: 'center', marginTop: '2rem' }}>Welcome to VYANA</h1>;
+};
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
