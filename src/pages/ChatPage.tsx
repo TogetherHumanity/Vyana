c0a27516e3fc3a77715732a1e9f319b7edb87d4d
TogetherHumanity <DAVID@TOGETHERHUMANITY.COM>
2025-06-16 19:28:52 -0700

    Delete src directory

--- src/pages/ChatPage.tsx
+++
@@ -1,3 +0,0 @@
-export default function ChatPage() {
-  return <div className="p-6 text-white text-xl">Welcome to VYANA Chat Assistant</div>;
-}
--- src/pages/TipsPage.tsx
+++
@@ -1,3 +0,0 @@
-export default function TipsPage() {
-  return <div className="p-6 text-white text-xl">Mental health tips coming soon...</div>;
-}
